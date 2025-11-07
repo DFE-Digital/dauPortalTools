f23cff7a280afca65bbe0dd3a616ef4cf4d69457
Ben7 Smith <ben7.smith@education.gov.uk>
2025-11-07 17:36:13 +0000

    Fix: AI fix

--- R/school_render_info.R
+++ R/school_render_info.R
@@ -50,205 +50,126 @@
 
 school_render_overview <- function(urn) {
   start_time <- Sys.time()
-  dauPortalTools::log_event(glue::glue(
-    "Starting school_render_overview with urn: {urn}"
-  ))
+  dauPortalTools::log_event(glue::glue("Starting school_render_overview with urn: {urn}"))
 
   conn <- sql_manager("dit")
-
   db_schema_00c <- DBI::SQL(conf$schemas$db_schema_00c)
 
-  sql_command <- glue::glue_sql(
-    "
+  sql_command <- glue::glue_sql("
     SELECT URN AS urn
-    ,[EstablishmentName] AS school_name
-    ,[LA (name)] AS la
-    ,[GOR (name)] AS region
-    ,[TypeOfEstablishment (name)] AS school_type
-    ,[EstablishmentTypeGroup (name)] AS school_type_group
-    ,[OpenDate] AS open_date
-    ,[CloseDate] AS close_date
-    ,[ReasonEstablishmentClosed (name)] AS reason_closed
-    ,[PhaseOfEducation (name)] AS phase
-    ,[ReligiousCharacter (name)] AS religious_character
-    ,[Diocese (name)] AS diocese_name
-    ,[Gender (name)] AS gender
-    ,[NumberOfPupils] AS pupil_number
-    ,[PercentageFSM] AS perc_fsm
-    ,[Trusts (code)] AS trust_ref
-    ,[Trusts (name)] AS trust_name
-    ,[SchoolWebsite] AS school_website
+          ,[EstablishmentName] AS school_name
+          ,[LA (name)] AS la
+          ,[GOR (name)] AS region
+          ,[TypeOfEstablishment (name)] AS school_type
+          ,[EstablishmentTypeGroup (name)] AS school_type_group
+          ,[OpenDate] AS open_date
+          ,[CloseDate] AS close_date
+          ,[ReasonEstablishmentClosed (name)] AS reason_closed
+          ,[PhaseOfEducation (name)] AS phase
+          ,[ReligiousCharacter (name)] AS religious_character
+          ,[Diocese (name)] AS diocese_name
+          ,[Gender (name)] AS gender
+          ,[NumberOfPupils] AS pupil_number
+          ,[PercentageFSM] AS perc_fsm
+          ,[Trusts (code)] AS trust_ref
+          ,[Trusts (name)] AS trust_name
+          ,[SchoolWebsite] AS school_website
     FROM {db_schema_00c}.[Edubase]
     WHERE URN = {urn}
-    AND [DateStamp] = (SELECT MAX(DateStamp) FROM {db_schema_00c}.[Edubase])
-    ",
-    .con = conn
-  )
-  print(sql_command)
+      AND [DateStamp] = (SELECT MAX(DateStamp) FROM {db_schema_00c}.[Edubase])
+  ", .con = conn)
+
   summary_data <- tryCatch(
-    {
-      DBI::dbGetQuery(conn, sql_command)
-    },
+    DBI::dbGetQuery(conn, sql_command),
     error = function(e) {
-      dauPortalTools::log_event(glue::glue(
-        "Error fetching summary: {e$message}"
-      ))
-      return(data.frame(
-        urn = NA,
-        school_name = NA
-      ))
+      dauPortalTools::log_event(glue::glue("Error fetching summary: {e$message}"))
+      return(data.frame(urn = NA, school_name = NA))
     }
   )
 
-  # Build Long Format Data
+  if (nrow(summary_data) == 0 || is.na(summary_data$urn[1])) {
+    return(shinyGovstyle::gov_layout(
+      size = "two-thirds",
+      shinyGovstyle::heading_text("School Overview", size = "l"),
+      shiny::HTML("<p>No data available for this URN.</p>")
+    ))
+  }
+
   urn_val <- summary_data$urn[1]
   trust_ref_val <- summary_data$trust_ref[1]
 
   rows <- list()
 
-  # School Details
-  school_details <- c(
-    urn = summary_data$urn,
-    school_name = summary_data$school_name,
-    la = summary_data$la,
-    region = summary_data$region,
-    school_type = summary_data$school_type,
-    school_type_group = summary_data$school_type_group,
-    open_date = summary_data$open_date
+  rows[["School Details"]] <- c(
+    URN = urn_val,
+    Name = summary_data$school_name,
+    LA = summary_data$la,
+    Region = summary_data$region,
+    Type = summary_data$school_type,
+    Group = summary_data$school_type_group,
+    Opened = summary_data$open_date
   )
   if (!is.na(summary_data$close_date)) {
-    school_details <- c(
-      school_details,
-      close_date = summary_data$close_date,
-      reason_closed = summary_data$reason_closed
-    )
+    rows[["School Details"]] <- c(rows[["School Details"]],
+                                  Closed = summary_data$close_date,
+                                  Reason = summary_data$reason_closed)
   }
-  rows[["School Details"]] <- school_details
 
-  # Ofsted Tab
-  rows[["Ofsted"]] <- c(status = "Coming Soon")
+  rows[["Ofsted"]] <- c(Status = "Coming Soon")
 
-  # Trust Details
   if (!is.na(trust_ref_val)) {
-    trust_details <- c(
-      trust_ref = summary_data$trust_ref,
-      trust_name = summary_data$trust_name
+    rows[["Trust Details"]] <- c(
+      Trust Ref = trust_ref_val,
+      Trust Name = summary_data$trust_name
     )
-    rows[["Trust Details"]] <- trust_details
   }
 
-  # More School Details
-  more_details <- c(
-    phase = summary_data$phase,
-    religious_character = summary_data$religious_character,
-    diocese_name = summary_data$diocese_name,
-    gender = summary_data$gender,
-    pupil_number = summary_data$pupil_number,
-    perc_fsm = summary_data$perc_fsm
+  rows[["More Details"]] <- c(
+    Phase = summary_data$phase,
+    Religious = summary_data$religious_character,
+    Diocese = summary_data$diocese_name,
+    Gender = summary_data$gender,
+    Pupils = summary_data$pupil_number,
+    "% FSM" = summary_data$perc_fsm
   )
-  rows[["More School Details"]] <- more_details
 
-  # Important Links
-  important_links <- c(
-    school_website = summary_data$school_website,
-    ofsted_link = paste0(
-      "http://www.ofsted.gov.uk/inspection-reports/find-inspection-report/provider/ELS/",
-      urn_val
-    ),
-    gis_school_link = paste0(
-      "https://get-information-schools.service.gov.uk/Establishments/Establishment/Details/",
-      urn_val
-    )
+  links <- c(
+    Website = summary_data$school_website,
+    Ofsted = paste0("http://www.ofsted.gov.uk/inspection-reports/find-inspection-report/provider/ELS/", urn_val),
+    GIAS School = paste0("https://get-information-schools.service.gov.uk/Establishments/Establishment/Details/", urn_val)
   )
   if (!is.na(trust_ref_val)) {
-    important_links <- c(
-      important_links,
-      gis_trust_link = paste0(
-        "https://get-information-schools.service.gov.uk/Groups/Group/Details/",
-        trust_ref_val
-      )
-    )
+    links <- c(links,
+               GIAS Trust = paste0("https://get-information-schools.service.gov.uk/Groups/Group/Details/", trust_ref_val))
   }
-  rows[["Important Links"]] <- important_links
+  rows[["Important Links"]] <- links
 
-  final_df <- do.call(
-    rbind,
-    lapply(names(rows), function(tab) {
-      data.frame(
-        tabs = tab,
-        field = names(rows[[tab]]),
-        value = unname(rows[[tab]]),
-        stringsAsFactors = FALSE
-      )
-    })
-  )
+  tab_panels <- lapply(names(rows), function(tab_name) {
+    data <- rows[[tab_name]]
 
-  # Prepare UI
-  heading <- if (!is.null(summary_data$school_name)) {
-    glue::glue("{summary_data$school_name} ({urn_val})")
-  } else {
-    "Summary"
-  }
+    if (tab_name == "Important Links") {
+      data <- ifelse(grepl("^http", data),
+                     paste0("<a href='", data, "' target='_blank'>", names(data), "</a>"),
+                     data)
+    }
 
-  # Split by tab for govTabs
-  tab_list <- split(final_df, final_df$tabs)
-
-  gov_tabs_ui <- do.call(
-    shinyGovstyle::govTabs,
-    c(
-      list(inputId = "summary_table"),
-      lapply(names(tab_list), function(tab_name) {
-        data <- tab_list[[tab_name]]
-
-        # Make links clickable for Important Links
-        if (tab_name == "Important Links") {
-          data$value <- ifelse(
-            grepl("^http", data$value),
-            paste0(
-              "<a href='",
-              data$value,
-              "' target='_blank'>",
-              data$field,
-              "</a>"
-            ),
-            data$value
-          )
-        }
-
-        # ✅ Return a proper tabPanel object
-        shiny::tabPanel(
-          title = tab_name,
-          shiny::HTML(paste0(
-            "<ul>",
-            paste0(
-              "<li><strong>",
-              data$field,
-              ":</strong> ",
-              data$value,
-              "</li>",
-              collapse = ""
-            ),
-            "</ul>"
-          ))
-        )
-      })
+    shinyGovstyle::govTab(
+      title = tab_name,
+      content = shiny::HTML(paste0(
+        "<ul>",
+        paste0("<li><strong>", names(data), ":</strong> ", data, "</li>", collapse = ""),
+        "</ul>"
+      ))
     )
-  )
+  })
 
   ui <- shinyGovstyle::gov_layout(
     size = "two-thirds",
-    shinyGovstyle::heading_text(heading, size = "l"),
-    shinyGovstyle::label_hint(
-      "summary_label",
-      "Key metrics for Warning Notices"
-    ),
-    gov_tabs_ui
+    shinyGovstyle::heading_text(glue::glue("{summary_data$school_name} ({urn_val})"), size = "l"),
+    shinyGovstyle::label_hint("school_overview_hint", "Key school information"),
+    do.call(shinyGovstyle::govTabs, c(list(inputId = "school_tabs"), tab_panels))
   )
 
-  end_time <- Sys.time()
-  dauPortalTools::log_event(glue::glue(
-    "Finished school_render_overview in {round(difftime(end_time, start_time, units = 'secs'), 2)} seconds"
-  ))
-
+  dauPortalTools::log_event(glue::glue("Finished school_render_overview in {round(difftime(Sys.time(), start_time, units = 'secs'), 2)} seconds"))
   return(ui)
 }
