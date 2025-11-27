89a29304c6bd280771ab462d375220a19afc3d73
Ben7 Smith <ben7.smith@education.gov.uk>
2025-11-27 10:42:42 +0000

    Bug: Added , eval.expr=TRUE to yaml load to stop warnings

--- R/log_transaction.R
+++ R/log_transaction.R
@@ -29,7 +29,7 @@ log_transaction <- function(
 ) {
   # Load confuration from YAML file
   library(yaml)
-  conf <- yaml::read_yaml("config.yml")
+  conf <- yaml::read_yaml("config.yml", eval.expr = TRUE)
   app_id <- conf$app_details$app_id
 
   schema_01a <- DBI::SQL(conf$schemas$db_schema_01a)
