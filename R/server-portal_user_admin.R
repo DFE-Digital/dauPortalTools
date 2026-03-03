192889cac3a55c4eeb3d752e2ea7177060cf37e7
Ben7 Smith <ben7.smith@education.gov.uk>
2026-03-03 13:06:42 +0000

    Feat: Added server-side code for user admin of portals - driven by app_id from conf

---
+++ R/server-portal_user_admin.R
@@ -0,0 +1,52 @@
+#' User/Role Management Server
+#'
+#' @param id Module ID
+#' @export
+server_portal_user_admin <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+
+    conf <- config_get()
+    app_id <- conf$app_details$app_id
+
+    username <- reactive(get_user(session = session, fallback = "admin"))
+
+    conn <- sql_manager("dit")
+
+    users_data <- reactive({
+      db_get_app_users(conn, app_id)
+    })
+
+    output$user_table <- renderDT({
+      datatable(
+        users_data(),
+        selection = "single",
+        rownames = FALSE,
+        options = list(pageLength = 20)
+      )
+    })
+
+    observeEvent(input$user_table_cell_double_click, {
+      row <- input$user_table_cell_double_click$row
+      df <- users_data()
+      req(row, df)
+      selected <- df[row, ]
+      showModal(ui_role_edit_modal(ns, selected, conn))
+    })
+
+    observeEvent(input$apply_role_change, {
+      req(input$selected_user_id, input$selected_role_id)
+
+      db_update_user_role(
+        conn = conn,
+        user_id = input$selected_user_id,
+        role_id = input$selected_role_id,
+        app_id = app_id,
+        assigned_by = username()
+      )
+
+      removeModal()
+      users_data(users_data())
+    })
+  })
+}
