1ffa4b80139805a45c13002439e10fc207c6b98c
Ben7 Smith <ben7.smith@education.gov.uk>
2026-03-11 09:49:32 +0000

    Fix: Repaired SQL query

--- R/quality_get_data.R
+++ R/quality_get_data.R
@@ -27,55 +27,46 @@
 #'
 
 quality_get_data <- function(record = NULL) {
-  # Log the start of the function
   log_event("Starting function quality_get_data")
 
   app_id <- conf$app_details$app_id
   db_schema_01a <- DBI::SQL(conf$schemas$db_schema_01a)
 
-  # Log the received record ID and app ID
   log_event(glue::glue("Received {record} id for app id: {app_id}."))
 
   conn <- sql_manager("dit")
 
   record <- if (!is.null(record)) {
     glue::glue_sql(
-      " AND l.record_id = '{record}'",
+      " AND l.record_id = {record}",
       .con = conn
     )
   } else {
     DBI::SQL("")
   }
 
-  # Construct the SQL query to retrieve quality data
-
   sql_command <- glue::glue_sql(
     "
     SELECT c.quality_name AS 'Quality Concern',
            c.quality_description AS 'Description',
-           [date_created] AS 'Date Identified',
-           [last_checked] AS 'Last Reviewed'
+           l.[date_created] AS 'Date Identified',
+           l.[last_checked] AS 'Last Reviewed'
     FROM {db_schema_01a}.[quality_list] l
     LEFT JOIN {db_schema_01a}.[quality_check] c 
         ON c.quality_check_id = l.error_id
-    LEFT JOIN {db_schema_01a}.[app_list] a 
-        ON l.app_id = a.app_id
     WHERE l.app_id = {app_id}
-      AND quality_status = 0
+      AND l.quality_status = 0
     {record}  ",
     .con = conn
   )
 
   summary_data <- DBI::dbGetQuery(conn, sql_command)
 
-  # Log the completion of data retrieval
-  log_event("Finished retrieving records")
-
-  # Log a structured summary of the retrieved data
   log_summary(quality_records)
 
   DBI::dbDisconnect(conn)
 
-  # Return the data frame of quality records
+  log_event("Finished function quality_get_data")
+
   return(quality_records)
 }
