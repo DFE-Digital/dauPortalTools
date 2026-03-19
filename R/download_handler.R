f1cfd70357f747c5c4f0105f6be035d76867331c
Ben7 Smith <ben7.smith@education.gov.uk>
2026-03-19 14:42:29 +0000

    Fix: Fixed issue that can cause namespace violations causing apps using download_handler to end

--- R/download_handler.R
+++ R/download_handler.R
@@ -51,70 +51,75 @@ download_handler <- function(
 
   rand_id <- paste0(sample(c(letters, 0:9), 6, TRUE), collapse = "")
   hash_id <- digest::digest(paste(file_label1, file_label2, rand_id))
-  dl_id <- paste0("dl_", hash_id)
+  dl_id <- paste0("auto_dl_", hash_id)
 
   session <- shiny::getDefaultReactiveDomain()
 
   if (!is.null(session)) {
-    session$output[[dl_id]] <- shiny::downloadHandler(
-      filename = function() {
-        paste0(
-          safe_label(file_label1),
-          if (!is.null(file_label2)) paste0("_", safe_label(file_label2)),
-          "_",
-          format(Sys.Date(), "%Y%m%d"),
-          ".csv"
-        )
-      },
-
-      content = function(file) {
-        data_to_write <- shiny::isolate(
-          if (shiny::is.reactive(df) || is.function(df)) df() else df
-        )
-
-        filename_final <- paste0(
-          safe_label(file_label1),
-          if (!is.null(file_label2)) paste0("_", safe_label(file_label2)),
-          "_",
-          format(Sys.Date(), "%Y%m%d"),
-          ".csv"
-        )
-
-        page_name_auto <- try(
-          {
-            infer_page_name(session)
+    session$onFlushed(
+      function() {
+        session$output[[dl_id]] <- shiny::downloadHandler(
+          filename = function() {
+            paste0(
+              safe_label(file_label1),
+              if (!is.null(file_label2)) paste0("_", safe_label(file_label2)),
+              "_",
+              format(Sys.Date(), "%Y%m%d"),
+              ".csv"
+            )
           },
-          silent = TRUE
-        )
 
-        if (inherits(page_name_auto, "try-error") || is.null(page_name_auto)) {
-          page_name_auto <- "Unknown Page"
-        }
-        shiny::withProgress(message = "Preparing your download...", value = 0, {
-          utils::write.csv(
-            data_to_write,
-            file,
-            row.names = FALSE,
-            na = ""
-          )
+          content = function(file) {
+            data_to_write <- shiny::isolate(
+              if (shiny::is.reactive(df) || is.function(df)) df() else df
+            )
 
-          incProgress(1)
-        })
+            filename_final <- paste0(
+              safe_label(file_label1),
+              if (!is.null(file_label2)) paste0("_", safe_label(file_label2)),
+              "_",
+              format(Sys.Date(), "%Y%m%d"),
+              ".csv"
+            )
 
-        record_download(
-          user = session$user %||% "Guest",
-          page_name = page_name_auto,
-          file_name = filename_final
+            page_name_auto <- try(infer_page_name(session), silent = TRUE)
+            if (
+              inherits(page_name_auto, "try-error") || is.null(page_name_auto)
+            ) {
+              page_name_auto <- "Unknown Page"
+            }
+
+            shiny::withProgress(
+              message = "Preparing your download...",
+              value = 0,
+              {
+                utils::write.csv(
+                  data_to_write,
+                  file,
+                  row.names = FALSE,
+                  na = ""
+                )
+                incProgress(1)
+              }
+            )
+
+            record_download(
+              user = session$user %||% "Guest",
+              page_name = page_name_auto,
+              file_name = filename_final
+            )
+          },
+
+          contentType = "text/csv"
         )
       },
-
-      contentType = "text/csv"
+      once = TRUE
     )
   }
 
   shiny::downloadButton(
-    dl_id,
-    "Download CSV",
+    outputId = dl_id,
+    label = "Download CSV",
     class = "govuk-button govuk-button--secondary"
   )
 }
