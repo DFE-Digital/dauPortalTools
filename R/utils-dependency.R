f23dc02df3259e9045375e360c71ba92afb48081
Ben7 Smith <ben7.smith@education.gov.uk>
2026-05-07 22:55:35 +0100

    Fix: Removing awful gemini suggestion

--- R/utils-dependency.R
+++
@@ -1,9 +0,0 @@
-#' @export
-ui_news_dependencies <- function() {
-  # This returns the specific HTML dependencies for DataTables
-  # without rendering an actual table.
-  htmltools::tagList(
-    DT::datatable(matrix(0, 0, 0)) |>
-      htmlwidgets::getDependency("datatables", "DT")
-  )
-}
