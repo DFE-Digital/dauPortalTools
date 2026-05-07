ac0ba85d8934f09dc2a40b3871548b706692d02a
Ben7 Smith <ben7.smith@education.gov.uk>
2026-05-07 17:44:36 +0100

    Refactor: Changed format of code to return ui element correclty

--- R/ui-show_news.R
+++ R/ui-show_news.R
@@ -40,15 +40,14 @@
 #'
 #' @export
 
-ui_show_news <- function() {
-  dauPortalTools::log_event("Starting ui_show_news")
+ui_show_news <- function(id = "news") {
+  session <- shiny::getDefaultReactiveDomain()
 
-  out <- tryCatch(
-    {
+  if (!is.null(session)) {
+    session$output[[id]] <- DT::renderDT({
       df <- db_get_portal_messages()
 
       if (is.null(df) || nrow(df) == 0) {
-        dauPortalTools::log_event("ui_show_news: no messages found")
         return(
           DT::datatable(
             data.frame(Message = "No news or alerts available"),
@@ -69,9 +68,7 @@ ui_show_news <- function() {
 
       df$new_badge <- ifelse(
         df$is_new,
-        "<span style='background:#005ea5;color:white;
-                padding:2px 6px;border-radius:3px;
-                font-size:0.75em;margin-right:6px'>NEW</span>",
+        "<span style='background:#005ea5;color:white;padding:2px 6px;border-radius:3px;font-size:0.75em;margin-right:6px'>NEW</span>",
         ""
       )
 
@@ -80,7 +77,7 @@ ui_show_news <- function() {
         function(x) {
           txt <- gsub("<[^>]+>", "", x)
           if (nchar(txt) > 180) {
-            paste0(substr(txt, 1, 180), "…")
+            paste0(substr(txt, 1, 180), "...")
           } else {
             txt
           }
@@ -89,12 +86,11 @@ ui_show_news <- function() {
       )
 
       df$display_text <- paste0(df$new_badge, df$preview_text)
-
-      df$expand_icon <- "\u25B6" # ▶
+      df$expand_icon <- "\u25B6"
 
       df <- df[order(df$priority, df$message_date, decreasing = FALSE), ]
 
-      dt <- DT::datatable(
+      DT::datatable(
         df[, c(
           "expand_icon",
           "message_date",
@@ -105,78 +101,16 @@ ui_show_news <- function() {
         )],
         rownames = FALSE,
         escape = FALSE,
-        colnames = c(
-          "",
-          "Date",
-          "Message",
-          "FullMessage",
-          "Priority",
-          "Posted by"
-        ),
         options = list(
           pageLength = 5,
           lengthChange = FALSE,
           searching = FALSE,
           ordering = FALSE,
-          dom = "tp",
-          columnDefs = list(
-            list(targets = c(3, 4), visible = FALSE),
-            list(targets = 0, width = "20px")
-          ),
-          rowCallback = DT::JS(
-            "
-            function(row, data) {
-              var priority = data[4];
-
-              if (priority == 1) {
-                $(row).css({'background-color':'#fdecea'});
-              } else if (priority == 2) {
-                $(row).css({'background-color':'#fff4e5'});
-              }
-            }
-            "
-          ),
-          callback = DT::JS(
-            "
-            table.on('click', 'tbody tr', function () {
-              var row = table.row(this);
-              var iconCell = $('td:eq(0)', this);
-
-              if (row.child.isShown()) {
-                row.child.hide();
-                iconCell.html('\\u25B6'); // ▶
-                $(this).removeClass('shown');
-              } else {
-                row.child(
-                  '<div style=\"padding:10px\">' + row.data()[3] + '</div>'
-                ).show();
-                iconCell.html('\\u25BC'); // ▼
-                $(this).addClass('shown');
-              }
-            });
-            "
-          )
+          dom = "tp"
         )
       )
+    })
+  }
 
-      dauPortalTools::log_event(
-        glue::glue("Finished ui_show_news ({nrow(df)} messages available)")
-      )
-
-      htmltools::as.tags(dt)
-    },
-    error = function(e) {
-      dauPortalTools::log_event(
-        paste0("ui_show_news error: ", conditionMessage(e))
-      )
-
-      DT::datatable(
-        data.frame(Message = "Unable to load news and alerts at this time"),
-        rownames = FALSE,
-        options = list(dom = "t")
-      )
-    }
-  )
-
-  out
+  DT::DTOutput(id)
 }
