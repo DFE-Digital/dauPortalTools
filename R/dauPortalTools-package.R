7efa4b0d63bece9286ab5395cb301102843feea9
Ben7 Smith <ben7.smith@education.gov.uk>
2026-05-07 14:52:33 +0100

    Docs: Updated tags

--- R/dauPortalTools-package.R
+++ R/dauPortalTools-package.R
@@ -1,21 +1,11 @@
 #' dauPortalTools: Internal Shiny Utilities for DfE Portals
 #'
-#' Provides a set of tools for building Shiny applications, including:
+#' Provides tools for building Shiny applications, including:
 #' \itemize{
-#'   \item Database access helpers
+#'   \item Database helpers
 #'   \item Logging utilities
 #'   \item GOV.UK-styled UI components
-#'   \item Reporting and dashboard helpers
 #' }
 #'
-#' @section Core Features:
-#' \itemize{
-#'   \item Centralised SQL access via `sql_manager()`
-#'   \item Structured logging via `log_event()`
-#'   \item Config-driven behaviour via `get_config()`
-#'   \item Standardised UI components for consistent UX
-#' }
-#'
-#' @docType package
-#' @name dauPortalTools
-NULL
+#' @keywords internal
+"_PACKAGE"
