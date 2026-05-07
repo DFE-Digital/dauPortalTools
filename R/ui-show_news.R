7f878b5c95f033e3c6d79a687f624989c07288fd
Ben7 Smith <ben7.smith@education.gov.uk>
2026-05-07 17:38:22 +0100

    Fix: Readded tags

--- R/ui-show_news.R
+++ R/ui-show_news.R
@@ -163,7 +163,7 @@ ui_show_news <- function() {
         glue::glue("Finished ui_show_news ({nrow(df)} messages available)")
       )
 
-      dt
+      htmltools::as.tags(dt)
     },
     error = function(e) {
       dauPortalTools::log_event(
