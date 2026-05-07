c1b8614826a9a995a18637c16eb5a959b4bad961
Ben7 Smith <ben7.smith@education.gov.uk>
2026-05-07 13:18:23 +0100

    Test: Rewrote tests

--- tests/testthat/test-db-portal_user_admin_helpers.R
+++ tests/testthat/test-db-portal_user_admin_helpers.R
@@ -1,3 +1,5 @@
+# tests/testthat/test-db-portal_user_admin_helpers.R
+
 test_that("db_get_app_users returns user-role data", {
   fake_df <- data.frame(
     user_id = c(1, 2),
@@ -8,43 +10,62 @@ test_that("db_get_app_users returns user-role data", {
     stringsAsFactors = FALSE
   )
 
-  out <- db_get_app_users(
-    app_id = 7,
-    db_get_query = function(conn, query) fake_df
-  )
+  testthat::with_mocked_bindings(
+    utils_db_get_query = function(conn, query) fake_df,
+    utils_get_app_id = function() 7,
+    utils_resolve_schema = function(...) "dbo",
+    sql_manager = function(...) structure(list(), class = "fake_conn"),
+    {
+      out <- db_get_app_users()
 
-  expect_equal(nrow(out), 2)
-  expect_equal(out$username[1], "alice")
+      expect_equal(nrow(out), 2)
+      expect_equal(out$username[1], "alice")
+      expect_equal(out$role_name[2], "User")
+    }
+  )
 })
 
 
 test_that("db_get_roles returns roles", {
   fake_df <- data.frame(
     role_id = c(1, 2),
-    role_name = c("Admin", "User")
+    role_name = c("Admin", "User"),
+    stringsAsFactors = FALSE
   )
 
-  out <- db_get_roles(
-    db_get_query = function(conn, query) fake_df
-  )
+  testthat::with_mocked_bindings(
+    utils_db_get_query = function(conn, query) fake_df,
+    utils_resolve_schema = function(...) "dbo",
+    sql_manager = function(...) structure(list(), class = "fake_conn"),
+    {
+      out <- db_get_roles()
 
-  expect_equal(out$role_name[2], "User")
+      expect_equal(nrow(out), 2)
+      expect_equal(out$role_name[2], "User")
+    }
+  )
 })
 
 
 test_that("db_update_user_role executes update", {
   executed <- FALSE
 
-  db_update_user_role(
-    user_id = 1,
-    role_id = 2,
-    app_id = 7,
-    assigned_by = 3,
-    db_execute = function(conn, query) {
+  testthat::with_mocked_bindings(
+    utils_db_execute = function(conn, query) {
       executed <<- TRUE
       invisible(1L)
+    },
+    utils_resolve_schema = function(...) "dbo",
+    sql_manager = function(...) structure(list(), class = "fake_conn"),
+    {
+      db_update_user_role(
+        user_id = 1,
+        role_id = 2,
+        app_id = 7,
+        assigned_by = 3
+      )
+
+      expect_true(executed)
     }
   )
-
-  expect_true(executed)
 })
